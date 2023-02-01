89fdc915230f9cdba918651b4185cc0bbc6f7482
Luca-Giudice <2giudice4@gmail.com>
2023-02-01 10:58:40 +0100

    Appheader

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import Appheader from './components/Appheader.vue';
+export default {
+  name: 'Ava & Associates',
+  components: {AppHeader}
+}
+</script>
+
+<template>
+<app-header></app-header>
+
+</template>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+  min-height: 100vh;
+}
+
+img{
+max-width: 100%;
+height: auto;
+
+}
+
+a{
+  text-decoration: none;
+}
+
+.container{
+  margin: 0 auto;
+  max-width: 1140px;
+}
+</style>
---
+++ src/components/Appheader.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+name: 'AppHeader'
+
+};
+</script>
+<template>
+<header>
+    <p>Avada & Associates legal team announce deal to open up oil & gas</p>
+    <ul>
+        <li>
+            <a href=""><i class="fa-brands fa-square-facebook"></i></a>
+        </li>
+        <li>
+            <a href=""><i class="fa-brands fa-square-twitter"></i></a>
+        </li>
+        <li>
+            <a href=""><i class="fa-brands fa-square-instagram"></i></a>
+        </li>
+        <li>
+            <a href=""><i class="fa-brands fa-square-youtube"></i></a>
+        </li>
+    </ul>
+
+</header>
+
+</template>
+
+<style scoped>
+ header{
+    display: flex;
+
+ };
+
+ ul{
+    display: flex;
+ }
+
+</style>
